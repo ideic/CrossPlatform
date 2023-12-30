207d7bb7b0037793d996f6cd18a9e5329872fadb
ideic <ideic75@gmail.com>
2023-12-30 17:19:41 +0100

    linux clang and gcc

--- RTPApp/RTPApp/RTPApp.cpp
+++ RTPApp/RTPApp/RTPApp.cpp
@@ -3,21 +3,13 @@
 
 #include "RTPApp.h"
 #include <optional>
+#include <string>
 using namespace std;
-//#include <unistd.h>
-//#include <limits.h>
 
 
 int main()
 {
-	//char hostname[HOST_NAME_MAX];
-	//char username[LOGIN_NAME_MAX];
-	//gethostname(hostname, HOST_NAME_MAX);
-	//getlogin_r(username, LOGIN_NAME_MAX);
-	
-	//cout << "Hello CMake."<< hostname << " uname:"<< username << endl;
 	optional<string> test17{ " 17 Passed 2." };
 	cout << "Hello CMake." << test17.value() << endl;
-	//std::cin.get();
 	return 0;
 }
