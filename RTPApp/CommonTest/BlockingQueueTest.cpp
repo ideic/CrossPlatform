a188a05d7b4bc0a007047d30b9f679112a926e8f
ideic <ideic75@gmail.com>
2024-01-30 13:00:50 +0100

    Better Blocing queue for one publiser, one subscriber

--- RTPApp/CommonTest/BlockingQueueTest.cpp
+++ RTPApp/CommonTest/BlockingQueueTest.cpp
@@ -3,13 +3,14 @@
 #include <chrono>
 #include <thread>
 #include <vector>
+#include "TrackNew.h"
 using namespace Xaba;
 using namespace std::chrono_literals;
 // Demonstrate some basic assertions.
 TEST(BlockingQueueTest, MeasureThroughput) {
 	BlockingQueue<std::vector<uint8_t>> queue;
+	TrackNew::reset();
 	auto start = std::chrono::system_clock::now();
-
 	std::thread producer([&queue]() {
 		
 		for (int i = 0; i < 800'000; i++) // 800'000 * 1500 * 8 = 9.6Gbps
@@ -31,24 +32,56 @@ TEST(BlockingQueueTest, MeasureThroughput) {
 		}
 	};
 	std::thread consumer1(consumer);
-	std::thread consumer2(consumer);
-	std::thread consumer3(consumer);
 
 	producer.join();
 
 	if (consumer1.joinable()) {
-		for (size_t i = 0; i < 10; i++)
-		{
-			queue.push({});
-		}
+		queue.push({});
 		consumer1.join();
 	}
-	if (consumer2.joinable()) consumer2.join();
-	if (consumer3.joinable()) consumer3.join();
 	auto end = std::chrono::system_clock::now();
+	TrackNew::status();
 	EXPECT_LT(std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count(), 1000);//9.6Gbps
 }
 
+TEST(BlockingQueueTest, MeasureThroughput2) {
+	BlockingQueue2<std::vector<uint8_t>> queue;
+	TrackNew::reset();
+	auto start = std::chrono::system_clock::now();
+	std::thread producer([&queue]() {
+
+		for (int i = 0; i < 800'000; i++) // 800'000 * 1500 * 8 = 9.6Gbps
+		{
+			std::vector<uint8_t> data(1500);
+			queue.push(std::move(data));
+		}
+		});
+
+	auto consumer = [&queue]() {
+		std::vector<uint8_t> data;
+		while (true)
+		{
+			data = queue.getNext();
+			if (data.empty())
+			{
+				break;
+			}
+		}
+		};
+	std::thread consumer1(consumer);
+
+	producer.join();
+
+	if (consumer1.joinable()) {
+		
+		queue.push({});
+		
+		consumer1.join();
+	}
+	auto end = std::chrono::system_clock::now();
+	TrackNew::status();
+	EXPECT_LT(std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count(), 1000);//9.6Gbps
+}
 TEST(BlockingQueueTest, push_get_next) {
 	
 	BlockingQueue<std::vector<uint8_t>> queue;
--- RTPApp/common/BlockingQueue.h
+++ RTPApp/common/BlockingQueue.h
@@ -33,3 +33,69 @@ namespace Xaba {
 		return result;
 	}
 };
+#include <deque>
+#include <memory_resource>
+#include <array>
+#include <optional>
+#include <atomic>
+#include <thread>
+namespace Xaba {
+	template<typename VALUE_TYPE>
+	class BlockingQueue2 {
+		//std::dequeue<VALUE_TYPE> items;
+	//	std::condition_variable cv;
+	//	std::mutex mtx;
+
+		//std::vector<uint8_t> _buf;// (6'000'000, 0);// , 60'000> _buf{};
+		constexpr static size_t bufSize_ = 30'000;
+		std::pmr::monotonic_buffer_resource _monotonicPool{bufSize_};// { _buf.data(), _buf.size() };
+		std::pmr::unsynchronized_pool_resource _pool{ &_monotonicPool };
+		std::pmr::vector<VALUE_TYPE> _items{&_pool };
+
+		std::atomic<size_t> size_{ 0 };
+		size_t upperIdx_{ 0 };
+		size_t lowIdx_{ 0 };
+	public:
+		void push(VALUE_TYPE &&data);
+		VALUE_TYPE getNext();
+
+		BlockingQueue2() {
+			_items.resize(bufSize_);
+		}
+		//BlockingQueue2() {
+		//	_buf.resize(2'000'000'000);
+		//	_pool.emplace(_buf.data(), _buf.size());
+		//	_items = std::pmr::deque<VALUE_TYPE>(&_pool.value());
+		//}
+	};
+	template<typename VALUE_TYPE>
+	void BlockingQueue2<VALUE_TYPE>::push(VALUE_TYPE &&data) {
+		size_t size = size_;
+		_items[++upperIdx_] = std::move(data);
+		while (!std::atomic_compare_exchange_strong(&size_, &size, size +1)) {
+			size = size_;
+		};
+		if (upperIdx_ == bufSize_-1) {
+			upperIdx_ = 0;
+		}
+		//std::unique_lock<std::mutex> lock(mtx);
+	}
+
+	template<typename VALUE_TYPE>
+	VALUE_TYPE BlockingQueue2<VALUE_TYPE>::getNext() {
+		VALUE_TYPE result;
+		size_t size = size_;
+		while (size == 0) {
+			std::this_thread::sleep_for(10ms);
+			size = size_;
+		}
+		while (!std::atomic_compare_exchange_strong( &size_, &size, size - 1)) {
+			size = size_;
+		};
+		result = std::move(_items[lowIdx_++]);
+		if (lowIdx_ == bufSize_-1) {
+			lowIdx_ = 0;
+		}
+		return result;
+	}
+};
